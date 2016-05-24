91cde4db713cefd8fc67a4eb0e9ecb135e08f279
Travis Montgomery <travisbm@gmail.com>
2016-05-24 18:00:43 -0500

    add nav-bar and authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,14 @@ class ApplicationController < ActionController::Base
 
   respond_to :json
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def angular
     render 'layouts/application'
+  end
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 end
